83f09953a539f69958bfcfb83fe1c70b3def72be
Anoushka210 <anoush2510@gmail.com>
2026-04-12 23:37:06 +0530

    shifted the main control to InventoryApplication.java

--- src/main/java/com/inventory/InventoryApplication.java
+++ src/main/java/com/inventory/InventoryApplication.java
@@ -3,7 +3,7 @@ package com.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication // This is the magic annotation that configures everything
+@SpringBootApplication 
 public class InventoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(InventoryApplication.class, args);
